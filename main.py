a4573f0be183e97248b90a18b9b21fdd8b7285e9
Michael Kerr <kerrjaxm@gmail.com>
2024-07-04 11:47:21 -0400

    added clearer comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-# declare customers name
+# declarations
 name = input("Welcome to GC Mini golf! What is your name? ")
 numOfPutts = 0
 totalScore = 0
@@ -8,11 +8,11 @@ par = 3
 
 print(f"Hi, {name}!", end=" ")
 
-# input for how many holes the user will play
+# loop input validation for how many holes the user will play
 while True:
     numOfHoles = int(input("Would you like to play 3 or 6 holes today? "))
 
-# loop for the number of holes played and what they putted
+# loop for what users score is
 
     if numOfHoles == 3 or numOfHoles == 6:
         for x in range(numOfHoles):
@@ -23,6 +23,7 @@ while True:
     else:
         print("That is not a valid entry. Please choose either 3 or 6 holes.")
 
+# output the users score
 if totalScore != 0:
     print(f"Good game, {name}! Your total score was: {totalScore:+}")
 else:
